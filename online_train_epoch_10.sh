dbfc2cdce5f65b4b58ebb39f88c7eb7b01757c93
Jaeyeop Lee <birdman9391@gmail.com>
2019-05-25 19:14:23 +0900

    adding some tools

---
+++ online_train_epoch_10.sh
@@ -0,0 +1,41 @@
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName dogs-jump --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName drift-chicane --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName drift-straight --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName goat --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName gold-fish --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName horsejump-high --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName india --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName judo --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName kite-surf --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName lab-coat --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName libby --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName loading --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName mbike-trick --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName motocross-jump --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName paragliding-launch --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName parkour --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName pigs --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName scooter-black --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName shooting --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
+python training/train_online_epoch_10.py --NoLabels 2 --lr 0.0025 --wtDecay 0.0001 --seqName soapbox --parentEpochResume 22 --epochsOnline 2 --noIterations 5 --batchSize 2
+
